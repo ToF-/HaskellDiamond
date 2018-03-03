23fb6856da126cde3eafa3e478b74a035e13cd80
ToF <cthibaut@octo.com>
2018-03-03 22:05:19 +0100

    identifying where the length comes from

--- Diamond.hs
+++ Diamond.hs
@@ -1,4 +1,5 @@
 module Diamond where
+import Data.Char (ord)
 
 diamond c = mirror (map mirror (chain c))
 
@@ -6,6 +7,7 @@ mirror xs = xs ++ tail (reverse xs)
 
 chain 'A' = ["A"]
 chain 'B' = [" A","B "]
-chain 'C' = [drop 0 (replicate 2 ' ' ++ ['A'] ++ replicate 0 ' '),
-             drop 1 (replicate 2 ' ' ++ ['B'] ++ replicate 1 ' '),
-             drop 2 (replicate 2 ' ' ++ ['C'] ++ replicate 2 ' ')]
+chain 'C' = [drop 0 (replicate l ' ' ++ ['A'] ++ replicate 0 ' '),
+             drop 1 (replicate l ' ' ++ ['B'] ++ replicate 1 ' '),
+             drop 2 (replicate l ' ' ++ ['C'] ++ replicate 2 ' ')]
+    where l = ord 'C' - ord 'A'
