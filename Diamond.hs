46aa88e0ee9090726dfbf920d9ce9d9c325945df
ToF <cthibaut@octo.com>
2018-03-03 22:03:02 +0100

    showing the patterns of padding

--- Diamond.hs
+++ Diamond.hs
@@ -6,4 +6,6 @@ mirror xs = xs ++ tail (reverse xs)
 
 chain 'A' = ["A"]
 chain 'B' = [" A","B "]
-chain 'C' = ["  A"," B ","C  "]
+chain 'C' = [drop 0 (replicate 2 ' ' ++ ['A'] ++ replicate 0 ' '),
+             drop 1 (replicate 2 ' ' ++ ['B'] ++ replicate 1 ' '),
+             drop 2 (replicate 2 ' ' ++ ['C'] ++ replicate 2 ' ')]
