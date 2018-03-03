1cc6f66b2c00fb10c8aa2aac9af2717f8b453382
ToF <cthibaut@octo.com>
2018-03-03 22:11:21 +0100

    last refactoring

--- Diamond.hs
+++ Diamond.hs
@@ -7,4 +7,5 @@ mirror xs = xs ++ tail (reverse xs)
 
 chain c = zipWith pad [0..] ['A'..c]
     where l = ord c - ord 'A'
-          pad n c = drop n (replicate l ' ' ++ [c] ++ replicate n ' ') 
+          pad n c = drop n (spaces l ++ [c] ++ spaces n) 
+          spaces = flip replicate ' ' 
