c07fc39cd75de4ef85d41944d99b91aa6243d110
ToF <cthibaut@octo.com>
2018-03-03 22:08:06 +0100

    extract method and zip with this method

--- Diamond.hs
+++ Diamond.hs
@@ -7,7 +7,6 @@ mirror xs = xs ++ tail (reverse xs)
 
 chain 'A' = ["A"]
 chain 'B' = [" A","B "]
-chain 'C' = [drop 0 (replicate l ' ' ++ ['A'] ++ replicate 0 ' '),
-             drop 1 (replicate l ' ' ++ ['B'] ++ replicate 1 ' '),
-             drop 2 (replicate l ' ' ++ ['C'] ++ replicate 2 ' ')]
+chain 'C' = zipWith pad [0..] ['A'..'C']
     where l = ord 'C' - ord 'A'
+          pad n c = drop n (replicate l ' ' ++ [c] ++ replicate n ' ') 
