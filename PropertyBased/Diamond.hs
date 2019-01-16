cecf466a92492de1ce54bc60b1c5c155ae300c94
ToF- <cthibaut@octo.com>
2019-01-16 21:45:59 +0100

    starts and ends with A

--- PropertyBased/Diamond.hs
+++ PropertyBased/Diamond.hs
@@ -1,4 +1,6 @@
 module Diamond where
+import Data.Char
 
 diamond :: Char -> [String]
-diamond _ = []
+diamond c = let h = ord c - ord 'A'
+    in replicate (h*2+1)  (replicate (h*2+1) 'A')
--- PropertyBased/Specs.hs
+++ PropertyBased/Specs.hs
@@ -13,3 +13,8 @@ main = do
     quickCheckOf "symmetry" $ forAll char $ 
         \c -> let d = diamond c 
                in d == reverse d && d == map reverse d
+
+    quickCheckOf "starts and ends with a centered A" $ forAll char $
+        \c -> let d = diamond c
+                  h = length d `div` 2
+              in d!!0!!h == 'A'
