f920abe815d0a533c4251e60dd244b6e3fe2e13d
ToF- <cthibaut@octo.com>
2019-01-17 17:28:21 +0100

    start again. First make a diamond generator

--- PropertyBased/Diamond.hs
+++ PropertyBased/Diamond.hs
@@ -2,14 +2,4 @@ module Diamond where
 import Data.Char
 
 diamond :: Char -> [String]
-diamond max = mirror half
-    where
-    half     = map mirror pattern
-    mirror s = s ++ drop 1 (reverse s)
-    pattern  = map letter ['A'..max]
-    letter l = let i = index l in spaces (size - i) ++ [l] ++ spaces i
-    index  c = ord c - ord 'A'
-    spaces n = replicate n ' '
-    size     = ord max - ord 'A'
-
-    
+diamond _ = []
--- PropertyBased/Specs.hs
+++ PropertyBased/Specs.hs
@@ -1,43 +1,23 @@
 import Test.QuickCheck
 import Diamond
 import Data.List
-
-char :: Gen Char 
-char = choose ('A','Z')
+import Data.Char
 
 quickCheckOf :: String -> Property -> IO ()
 quickCheckOf s p = putStr (s ++ "\t") >> quickCheck p
 
+data Diamond = Diamond { letter :: Char, 
+                           size :: Int,   
+                          shape :: [String] }
+    deriving (Eq, Show)
+
+instance Arbitrary Diamond where
+    arbitrary = do
+        l <- choose ('A','Z')
+        let n = ord l - ord '@' 
+        return $ Diamond l (n*2-1) (diamond l)
+
 main = do
-    quickCheckOf "symmetry" $ forAll char $ 
-        \c -> let d = diamond c 
-               in d == reverse d 
-               && d == map reverse d
-
-    quickCheckOf "starts and ends with a centered A" $ forAll char $
-        \c -> let d = diamond c
-                  d'= reverse d
-                  h = length d `div` 2
-              in d!!0!!h == 'A' && d'!!0!!h == 'A'
-
-    quickCheckOf "has the final letter at both ends of the middle line" $ forAll char $
-        \c -> let d = diamond c
-                  h = length d `div` 2
-              in d!!h!!0 == c && (reverse d!!h)!!0 == c 
-
-    quickCheckOf "position of letter depends on index of letter" $ forAll char $
-        \c -> let d = diamond c
-                  h = length d `div` 2
-                  s = map (drop h) (take (h+1) d)
-              in all (\i -> s!!i!!i == ['A'..c]!!i) [0..h] 
-
-    quickCheckOf "any position that is not in the diagonal is a space" $ forAll char $
-        \c -> do
-            let d = diamond c
-            let h = length d `div` 2
-            let s = map (drop h) (take (h+1) d)
-            i <- choose (0,h)
-            j <- choose (0,h)
-            return $ (i /= j) == (s!!j!!i == ' ')
-                
-           
+    quickCheckOf "has size equal to letter * 2 - 1" 
+        $ forAll arbitrary $ 
+        \(Diamond c s shape) -> length shape == s
