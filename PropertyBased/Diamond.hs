92f16d4fc0e5fd9ae5344967aae026292edc6e07
ToF- <cthibaut@octo.com>
2019-01-17 18:32:09 +0100

    refactoring

--- PropertyBased/Diamond.hs
+++ PropertyBased/Diamond.hs
@@ -4,11 +4,10 @@ import Data.Char
 diamond :: Char -> [String]
 diamond l = let 
     max = ord l - ord 'A'
-    ur  = map letter ['A'..l] 
-    letter c = spaces n ++ [c] ++ spaces (max-n)
-        where 
-        n = ord c - ord 'A'
+    upperLeft  = map letter ['A'..l] 
+    letter c = spaces (max-n) ++ [c] ++ spaces n 
+        where n = ord c - ord 'A'
     spaces x = replicate x ' '
-    ul = map (reverse . drop 1) ur
-    half = zipWith (++) ul ur
-  in half ++ drop 1 (reverse half)
+    upperHalf = map mirror upperLeft
+    mirror s = s ++ drop 1 (reverse s)
+  in mirror upperHalf
